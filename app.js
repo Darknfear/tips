6cb35f4f9157b552cc7428f3d4eaf9f8ca1c5e57
hieu <hieuact13011998@gmail.com>
2023-05-23 15:43:14 +0700

    update config redis

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { set, get, setNx, decryBy, exists, incrBy, client } = require('./src/redis/redis.v1');
+
 // config
 dotenv.config();
 
 // init app
 const app = express();
 
+// config redis
+client.connect().then(() => {
+    console.log(`Connect redis success!`);
+}).catch(err => console.log(`error when connect redis ${err}`));
+
 // router
-app.get('/order', (req, res) => {
+app.get('/order', async (req, res) => {
     const time = new Date().getTime();
     console.log(`Time request::::: ${time}`);
 
@@ -17,9 +24,23 @@ app.get('/order', (req, res) => {
     const name = 'Iphone13';
 
     const sellNumber = 1;
+    console.log(`debug1`);
+    const key = await exists(name);
+    console.log(`debug`);
 
+    if (!key) {
+        await set(name, 0);
+    }
 
+    let boughtNumber = await get(name);
+    console.log(`Before user order success ${boughtNumber}`);
+    if (+boughtNumber + sellNumber > numInventory) {
+        console.log(`Out of stock`);
+        return res.json({ success: 'error', message: 'error'});
+    }
 
+    boughtNumber = await incrBy(name, 1);
+    console.log(`After user order success ${boughtNumber}`);
     return res.json({
         status: 'success',
         message: 'ok',
--- src/redis/redis.v1.js
+++ src/redis/redis.v1.js
@@ -2,62 +2,58 @@
 const redis = require('redis');
 const client = redis.createClient();
 
-const set = async (key, value) => new Promise((resolve, reject) => {
-    client.set(key, value, (err, result) => {
-        if (err) {
-            reject(err);
-        }
-        resolve(result);
-    });
+const set = async (key, value) => {
+    try {
+       return await client.set(key, value);
+    } catch (e) {
+        console.log(`Error when set value redis ${e}`);
+        throw e;
+    }
+};
 
-});
+const get = async (key, value) => {
+    try {
+        return await client.get(key, value);
+    } catch (e) {
+        console.log(`Error when get value redis ${e}`);
+        throw e;
+    }
+};
 
-const get = async (key, value) => new Promise((resolve, reject) => {
-    client.get(key, value, (err, result) => {
-        if (err) {
-            reject(err);
-        }
-        resolve(result);
-    });
+const incrBy = async (key, value) => {
+    try {
+        return await client.incrBy(key, value);
+    } catch (e) {
+        console.log(`Error when incrBy value redis ${e}`);
+        throw e;
+    }
+};
 
-});
+const decrBy = async (key, value) => {
+    try {
+        return await client.decrBy(key, value);
+    } catch (e) {
+        console.log(`Error when decryBy value redis ${e}`);
+        throw e;
+    }
+};
 
-const incrBy = async (key, value) => new Promise((resolve, reject) => {
-    client.incrBy(key, value, (err, result) => {
-        if (err) {
-            reject(err);
-        }
-        resolve(result);
-    });
+const exists = async (key) => {
+    try {
+        return await client.exists(key);
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+};
 
-});
+const setNx = async (key, value) => {
+    try {
+        return await client.setNX(key, value);
+    } catch (e) {
+        console.log(`Error when setNx value redis ${e}`);
+        throw e;
+    }
+};
 
-const decryBy = async (key, value) => new Promise((resolve, reject) => {
-    client.decrBy(key, value, (err, result) => {
-        if (err) {
-            reject(err);
-        }
-        resolve(result);
-    });
-
-});
-
-const exists = async (key) => new Promise((resolve, reject) => {
-    client.exists(key, (err, result) => {
-        if (err) {
-            reject(err);
-        }
-        resolve(result);
-    });
-});
-
-const setNx = async (key, value) => new Promise((resolve, reject) => {
-    client.setNX(key, value, (err, result) => {
-        if (err) {
-            reject(err);
-        }
-        resolve(result);
-    })
-});
-
-module.exports = {set, get, incrBy, decryBy, setNx, exists};
+module.exports = {set, get, incrBy, decrBy, setNx, exists, client };
